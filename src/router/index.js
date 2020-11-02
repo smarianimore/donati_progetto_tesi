d09c373f4de5b6265565556658547bcb1b8d01b1
Michele Donati <michele.donati5@studio.unibo.it>
2020-11-02 16:36:34 +0100

    Deleted unused component

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ConfigPage from "@/components/ConfigPage";
 import Home_top_first_row from "@/components/Patients_top_first_row";
 import Vehicles_left_and_map from "@/components/Vehicles_left_and_map";
 
 Vue.use(Router)
 
 const routes = [
-    {
-        path: "/",
-        name: 'ConfigPage',
-        component: ConfigPage
-    },
     {
         path: "/home_patients",
         name: 'Home_top_first_row',
